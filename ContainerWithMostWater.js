1183407c2fb0959211c0da7e363149ee12e0de94
Bruce Qin <qinfeng21@hotmail.com>
2022-01-27 20:24:36 -0800

    upload

---
+++ ContainerWithMostWater.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let i = 0;
+  let j = height.length - 1;
+  let max = 0;
+  while (i < j) {
+    area = (j - i) * Math.min(height[i], height[j]);
+    max = Math.max(max, area);
+    height[i] < height[j] ? i++ : j--;
+  }
+  return max;
+};
